0dffb52ee30c64ab3d727205e5d2a749d9fd65a5
washtakzo <washtakzo@gmail.com>
2022-07-14 12:19:16 +0200

    grid and paper testing ...

---
+++ src/components/work-space/WorkSpace.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './WorkSpace.module.css'
+import { Grid, Paper } from '@mui/material'
+
+const WorkSpace = () =>{
+    return<Grid container>
+        <Grid item>
+            <Paper>
+                Test
+            </Paper>
+        </Grid>
+    </Grid>
+}
+
+export default WorkSpace;
