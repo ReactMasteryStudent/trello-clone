4f6878ed958c038c67082939412067c15e074358
casper <otmanicasper@gmail.com>
2022-08-09 22:17:37 +0100

    header board style

--- src/pages/tab-detail/TabDetail.jsx
+++ src/pages/tab-detail/TabDetail.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 //import { useParams } from "react-router-dom";
-//import { Column } from "../../components/Column/ColumnBoard";
+import { Column } from "../../components/Column/ColumnBoard";
+import { HeaderBoard } from "../../components/header-board/HeaderBoard";
 
 const TabDetail = () => {
   //const params = useParams();
-  return 
-      //<Column />
-  
+  return (
+    <>
+      <HeaderBoard />
+      <Column />
+    </>
+      
+  );
 };
 
 export default TabDetail;
