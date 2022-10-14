1bd11ce3b2d443d09d4f34f6c74b7ec32cbfe75a
casper <otmanicasper@gmail.com>
2022-10-14 12:28:19 +0100

    fix key bug on workspace

--- src/components/work-space/WorkSpace.jsx
+++ src/components/work-space/WorkSpace.jsx
@@ -5,14 +5,14 @@ import TabList from "../tab-list/TabList";
 const WorkSpace = (props) => {
   return (
     <>
-      {props.workSpaces.map((workSpace)=>{
+      {props.workSpaces.map((workSpace, index)=>{
         // Pour chaque workSpace j'affiche son name et ses boards
-        return <>
+        return <React.Fragment key={index}>
           <div className={styles.title}>
             <h6>{workSpace.name}</h6>
           </div>
           <TabList list={workSpace.boards} />
-          </>
+          </React.Fragment>
       })}
     </>
   );
